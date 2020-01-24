203a106ef12d48966f9066242675d992fd308043
Ryan Conaway <mc321015@ohio.edu>
2020-01-24 15:05:25 -0500

    Intial Commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Ryan Conaway
+ *        Date: January 24th 2020
+ * Description: displays a message in the terminal
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
